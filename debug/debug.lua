a8b8ad51febe064269786ccc7cd992532bd50d01
whilealive <bischofberger@fastmail.fm>
2022-04-25 21:17:08 +0200

    some debugging stuff

---
+++ debug/debug.lua
@@ -0,0 +1,13 @@
+-- ==================================================================
+-- FILE     debug.lua
+-- INFO     
+--
+-- DATE     25.04.2022
+-- OWNER    Bischofberger
+-- ==================================================================
+
+require('lfs')  -- install lua-filesystem
+
+require('statementCollator')
+
+print(lfs.currentdir())
